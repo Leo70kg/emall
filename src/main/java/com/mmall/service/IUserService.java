7489caf24ab9efa72e00297099058249b3bd9bfd
zuoyou <zuoyou1993@hotmail.com>
2020-07-26 23:31:05 +0100

    add Category Module

--- src/main/java/com/mmall/service/IUserService.java
+++ src/main/java/com/mmall/service/IUserService.java
@@ -9,17 +9,19 @@ public interface IUserService {
 
     ServerResponse<String> register(User user);
 
-    public ServerResponse<String> checkValid(String str, String type);
+    ServerResponse<String> checkValid(String str, String type);
 
-    public ServerResponse selectQuestion(String username);
+    ServerResponse selectQuestion(String username);
 
-    public ServerResponse<String> checkAnswer(String username, String question, String answer);
+    ServerResponse<String> checkAnswer(String username, String question, String answer);
 
-    public ServerResponse<String> forgetResetPassword(String username, String passwordNew, String forgetToken);
+    ServerResponse<String> forgetResetPassword(String username, String passwordNew, String forgetToken);
 
-    public ServerResponse<String> resetPassword(String passwordOld, String passwordNew, User user);
+    ServerResponse<String> resetPassword(String passwordOld, String passwordNew, User user);
 
-    public ServerResponse<User> updateInformation(User user);
+    ServerResponse<User> updateInformation(User user);
 
-    public ServerResponse<User> getInformation(Integer userId);
+    ServerResponse<User> getInformation(Integer userId);
+
+    ServerResponse checkAdminRole(User user);
 }
